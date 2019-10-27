b9ca21092856846a30c71a21ab2de931980236ca
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-27 22:22:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def consolidate_cart(cart)
  cart.each do |item_hash|
    item_hash.each do |name, price_hash|
      if hash[name].nil?
-       hash[name] = price_hash.merge({:count => 1})
+       hash[name] = 1
      else
        hash[name][:count] += 1
      end
