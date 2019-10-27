934b640be597b5bdf741624779544dcd8d8fecf5
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-27 23:52:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,21 +30,39 @@ def consolidate_cart(cart)
   result
 end
 
-def apply_coupons(cart, coupons)
-  index = 0
+def mk_coupon_hash(c)
+  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
+  {
+    :item => "#{c[:item]} W/COUPON",
+    :price => rounded_unit_price,
+    :count => c[:num]
+  }
+end
 
-  while index < cart.length do
-    coupon = coupons[index]
+# A nice "First Order" method to use in apply_coupons
+
+def apply_coupon_to_cart(matching_item, coupon, cart)
+  matching_item[:count] -= coupon[:num]
+  item_with_coupon = mk_coupon_hash(coupon)
+  item_with_coupon[:clearance] = matching_item[:clearance]
+  cart << item_with_coupon
+end
+
+def apply_coupons(cart, coupons)
+  i = 0
+  while i < coupons.count do
+    coupon = coupons[i]
     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
     item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
 
-    apply_count = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
-
-    if apply_count
+    if item_is_in_basket and count_is_big_enough_to_apply
+      apply_coupon_to_cart(item_with_coupon, coupon, cart)
+    end
+    i += 1
   end
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
+  cart
 end
 
 def apply_clearance(cart)
