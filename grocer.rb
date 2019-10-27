023731275de6972fb757fb7d89a51a25a9316412
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-27 22:20:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,28 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  index = 0
+
+  while index < collection.length do
+    if collection[index][:item] == name
+      return collection[index]
+    else
+      index += 1
+    end
+  end
+  return nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  hash = {}
+ cart.each do |item_hash|
+   item_hash.each do |name, price_hash|
+     if hash[name].nil?
+       hash[name] = price_hash.merge({:count => 1})
+     else
+       hash[name][:count] += 1
+     end
+   end
+ end
+ hash
 end
 
 def apply_coupons(cart, coupons)
@@ -34,3 +48,11 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
+
+cart = [{:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "BANANA", :price => 3.00, :clearance => true},
+  {:item => "CARROT", :price => 3.00, :clearance => true},
+  {:item => "TV", :price => 3.00, :clearance => true},
+  {:item => "XBOX", :price => 3.00, :clearance => true},
+  {:item => "CHOCOLATE", :price => 3.00, :clearance => true},
+  {:item => "CHOCOLATE", :price => 3.00, :clearance => true}]
